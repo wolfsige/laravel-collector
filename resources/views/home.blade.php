01df6fc23e8c37dee9858dc4c20947d28680b66f
Wolf <d.wolftripp@gmail.com>
2023-09-18 14:34:21 -0700

    add register mock

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/home.css') }}" rel="stylesheet">
   <title>Document</title>
 </head>
 <body>
-  <h1>Home</h1>
-  <p>Where the content goes.</p>
+  <div class="box">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+      @csrf
+
+      <input type="text" name="name" placeholder="Name">
+      <input type="text" name="email" placeholder="Email">
+      <input type="text" name="password" placeholder="Password">
+      <button>Confirm</button>
+    </form>
+  </div>
 </body>
 </html>
