244112c08118e4f4573547e1fc07b11ac5c770f1
Wolf <d.wolftripp@gmail.com>
2023-09-18 17:27:11 -0700

    add database connectivity

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,6 +13,10 @@ class UserController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required', 'min:8']
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
+
         return "hello from controller";
     }
 }
